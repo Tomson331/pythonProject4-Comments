30feb9bed5064aec9a856be6b18c16a66f3409df
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-05 15:30:00 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Day 3 - Comments
+
+# print("Hej Zero To Junior")
+
+# To jest komentarz
+print("Hello, World!")
+
+# To też jest komentarz
+# zapisany w kilku
+# liniach
+# print("To się na pewno wyświetli")
+
+# TODO
+# Dodać else do if’a
+print("To się na pewno nie wyświetli")
